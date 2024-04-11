d9f3d6d0b7a13119a3b20c169c57ec467c0f68e4
Nikita <nikitamasslennikov@mail.ru>
2024-04-11 20:07:43 +0300

    Initial commit

---
+++ src/main/java/ru/pet/my_banking_app/MyBankingAppApplication.java
@@ -0,0 +1,13 @@
+package ru.pet.my_banking_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBankingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBankingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/pet/my_banking_app/MyBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pet.my_banking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBankingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
